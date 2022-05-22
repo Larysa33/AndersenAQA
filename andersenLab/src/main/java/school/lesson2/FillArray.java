4eae8904d8818f12cea0beb637800dd691b075eb
Larysa33 <ligore@yahoo.com>
2022-05-23 00:39:49 +0300

    first commit for all methods

--- andersenLab/src/main/java/school/lesson2/FillArray.java
+++ andersenLab/src/main/java/school/lesson2/FillArray.java
@@ -6,8 +6,8 @@ public class FillArray {
     }
     public static void fillArray(){
         int [] arr1 = new int[100];
-        for (int i = 1; i < arr1.length; i++) {
-            arr1[i]=i;
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i]=i+1;
             System.out.print(arr1[i] + " ");
         }
         System.out.println();

ef48f366748c8d865d469293ee51dace48bffd29
Larysa33 <ligore@yahoo.com>
2022-05-29 21:21:43 +0300

    first commit

---
+++ andersenLab/src/main/java/school/lesson4/CatsApp.java
@@ -0,0 +1,10 @@
+package school.lesson4;
+
+public class CatsApp {
+    public static void Main(String[] args){
+        Cat Mefistophel = new Cat ("Мефистофель", 30);
+        Mefistophel.run(100);
+        Mefistophel.swim(200);
+        Mefistophel.animalInfo();
+    }
+}

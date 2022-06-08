7058405c87188a8cbc6c6031479e6626166f357d
Larysa33 <ligore@yahoo.com>
2022-06-08 23:28:26 +0300

    final commit (now everything works)

--- andersenLab/src/main/java/school/lesson6/ArrayApp.java
+++ andersenLab/src/main/java/school/lesson6/ArrayApp.java
@@ -1,35 +1,37 @@
 package school.lesson6;
 
 import static java.lang.Integer.parseInt;
-import static java.lang.String.*;
+//import static java.lang.String.*;
 
 public class ArrayApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        String table [][] = {{"2", "2", "2", "2"}};
+        String table[][] = {{"4", "5", "i", "1"}, {"1", "2", "1", "8"}, {"1", "2", "1", "2"}, {"2", "2", "2", "2"}};
         try {
-            checkArray(table);
+            int result = arraySum(table);
+            System.out.println(result);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("MyArraySizeException: неверный размер массива.");
+        } catch (NumberFormatException e) {
         }
     }
 
-    public static void checkArray(String[][] ignoredArr) {
-        String [][] arr = new String [4][4];
-            int [][] arr2 = new int [4][4];
-        try {
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr.length; j++) {
-                    arr2[j] = parseInt(arr[j]);
-                    arr2[i] = parseInt(arr[i]);
+    public static int arraySum(String[][] arr) {
+        int sum = 0;
+        if (arr.length != 4) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    sum = sum + Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    System.out.println("MyArrayDataException: некорректный тип данных");
+                    System.out.println("Ошибка в ячейке: " + i + "x" + j);
                 }
             }
-        } catch (NumberFormatException e) {
-            System.out.println("MyArrayDataException: " + e);
         }
-        int sum = 0;
-        for (int i = 0; i < arr2.length; i++) {
-            sum = sum + arr2[i];
-        }
+        System.out.println("Сумма всех элементов массива = " + sum);
+        return sum;
     }
 }
